418c756a374e052bec6d5231a43c84110cc40132
Peng Huang <hpeng526@gmail.com>
2016-03-30 21:45:12 +0800

    tab

--- Two Sum/TwoSum.java
+++ Two Sum/TwoSum.java
@@ -1,20 +1,20 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-    	int[] ans = {0, 0};
+        int[] ans = {0, 0};
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         map.clear();
-        for(int i=0; i<nums.length; i++) {
-        	map.put(nums[i], i);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
-        for(int i=0; i<nums.length; i++) {
-        	if(map.containsKey(target - nums[i])) {
-        		ans[0] = i;
-        		ans[1] = map.get(target - nums[i]);
-        		if(ans[0] == ans[1]) {
-        			continue;
-        		}
-        		break;
-        	}
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
+                if (ans[0] == ans[1]) {
+                    continue;
+                }
+                break;
+            }
         }
         return ans;
     }
