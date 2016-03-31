734269554f45c87e052719fb97618b64126afcc3
Peng Huang <hpeng526@gmail.com>
2016-03-31 21:45:08 +0800

    better

--- Add Two Numbers/AddTwoNumbers.java
+++ Add Two Numbers/AddTwoNumbers.java
@@ -9,49 +9,27 @@
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int add = 0;
-        ListNode head = null;
-        ListNode cur = null;
-        while(l1 != null && l2 != null) {
-            int val = l1.val + l2.val + add;
-            ListNode newNode = new ListNode(val % 10);
-            if(head == null) {
-                head = newNode;
-            } else {
-                cur.next = newNode;
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while(l1 != null || l2 != null) {
+            int sum = add;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            cur = newNode;
-            add = val / 10;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        while (l1 != null) {
-            int val = l1.val + add;
-            ListNode newNode = new ListNode(val % 10);
-            if(head == null) {
-                head = newNode;
-            } else {
-                cur.next = newNode;
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
-            cur = newNode;
-            add = val / 10;
-            l1 = l1.next;
-        }
-        while (l2 != null) {
-            int val = l2.val + add;
-            ListNode newNode = new ListNode(val % 10);
-            if(head == null) {
-                head = newNode;
-            } else {
-                cur.next = newNode;
-            }
-            cur = newNode;
-            add = val / 10;
-            l2 = l2.next;
+            add = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+
         }
         if (add > 0) {
-            ListNode newNode = new ListNode(add);
-            cur.next = newNode;
+            cur.next = new ListNode(add);
         }
-        return head;
+        return head.next;
     }
 }
