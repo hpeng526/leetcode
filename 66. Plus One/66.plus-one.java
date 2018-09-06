a470f8aa2452a04eab4d901de50ab45bb19eceab
hpeng526 <hpeng526@gmail.com>
2018-09-06 19:07:53 +0800

    update

---
+++ 66. Plus One/66.plus-one.java
@@ -0,0 +1,78 @@
+/*
+ * [66] Plus One
+ *
+ * https://leetcode.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (39.98%)
+ * Total Accepted:    283.2K
+ * Total Submissions: 708.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a non-empty array of digits representing a non-negative integer, plus
+ * one to the integer.
+ * 
+ * The digits are stored such that the most significant digit is at the head of
+ * the list, and each element in the array contain a single digit.
+ * 
+ * You may assume the integer does not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ * 
+ * 
+ */
+class Solution {
+    public int[] plusOne(int[] digits) {
+        // actually no need to cal
+        // if (digits[digits.length - 1] < 9) {
+        //     digits[digits.length - 1] += 1;
+        //     return digits;
+        // }
+        // int cur;
+        // for (cur = digits.length - 1; cur >= 0; cur--) {
+        //     if (digits[cur] != 9) {
+        //         break;
+        //     }
+        // }
+        // // not continue 9
+        // if (cur < 0) {
+        //     int[] res = new int[digits.length + 1];
+        //     res[0] = 1;
+        //     return res;
+        // }
+
+        // digits[cur] += 1;
+        // for (int i = cur + 1; i < digits.length; i++) {
+        //     digits[i] = 0;
+        // }
+        // return digits;
+
+        // polish
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        // head is 9
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
